e82f2d1b89fbb41beea0bb6585fa7cf867adb4e3
willyan <willyan.sergio.fidelis@gmail.com>
2020-03-04 01:03:56 -0300

    second commit

--- Usuarios.py
+++ Usuarios.py
@@ -14,72 +14,72 @@ class Usuarios(object):
         self.senha = senha
     
  
-def insertUser(self):
+    def insertUser(self):
+    
+        banco = Banco()
+        try:
+        
+            c = banco.conexao.cursor()
+        
+            c.execute("insert into usuarios (nome, telefone, email, usuario, senha) values ('" + self.nome + "', '" + self.telefone + "', '" + self.email + "', '" + self.usuario + "', '" + self.senha + "' )")
+        
+            banco.conexao.commit()
+            c.close()
+        
+            return "Usuario cadastrado com sucesso!"
+        except:
+            return "Ocorreu um erro na insercao do usuario"
+    
+    def updateUser(self):
+        
+        banco = Banco()
+        try:
+        
+            c = banco.conexao.cursor()
+        
+            c.execute("update usuarios set nome = '" + self.nome + "', telefone = '" + self.telefone + "', email = '" + self.email + "', usuario = '" + self.usuario + "', senha = '" + self.senha + "' where idusuario = " + self.idusuario + " ")
+        
+            banco.conexao.commit()
+            c.close()
+        
+            return "Usuario atualizado com sucesso!"
+        except:
+            return "Ocorreu um erro na alteracao do usuario"
+    
+    def deleteUser(self):
+        
+        banco = Banco()
+        try:
+        
+            c = banco.conexao.cursor()
+        
+            c.execute("delete from usuarios where idusuario = " + self.idusuario + " ")
+        
+            banco.conexao.commit()
+            c.close()
+        
+            return "Usuario excluido com sucesso!"
+        except:
+            return "Ocorreu um erro na exclusao do usuario"
+    
+    def selectUser(self, idusuario):
+        banco = Banco()
+        try:
+        
+            c = banco.conexao.cursor()
+        
+            c.execute("select * from usuarios where idusuario = " + idusuario + "  ")
+        
+            for linha in c:
+                self.idusuario = linha[0]
+                self.nome = linha[1]
+                self.telefone = linha[2]
+                self.email = linha[3]
+                self.usuario = linha[4]
+                self.senha = linha[5]
+        
+            c.close()
  
-  banco = Banco()
-  try:
- 
-      c = banco.conexao.cursor()
- 
-      c.execute("insert into usuarios (nome, telefone, email, usuario, senha) values ('" + self.nome + "', '" + self.telefone + "', '" + self.email + "', '" + self.usuario + "', '" + self.senha + "' )")
- 
-      banco.conexao.commit()
-      c.close()
- 
-      return "Usuario cadastrado com sucesso!"
-  except:
-      return "Ocorreu um erro na insercao do usuario"
- 
-def updateUser(self):
- 
-  banco = Banco()
-  try:
- 
-      c = banco.conexao.cursor()
- 
-      c.execute("update usuarios set nome = '" + self.nome + "', telefone = '" + self.telefone + "', email = '" + self.email + "', usuario = '" + self.usuario + "', senha = '" + self.senha + "' where idusuario = " + self.idusuario + " ")
- 
-      banco.conexao.commit()
-      c.close()
- 
-      return "Usuario atualizado com sucesso!"
-  except:
-      return "Ocorreu um erro na alteracao do usuario"
- 
-def deleteUser(self):
- 
-  banco = Banco()
-  try:
- 
-      c = banco.conexao.cursor()
- 
-      c.execute("delete from usuarios where idusuario = " + self.idusuario + " ")
- 
-      banco.conexao.commit()
-      c.close()
- 
-      return "Usuario excluido com sucesso!"
-  except:
-      return "Ocorreu um erro na exclusao do usuario"
- 
-def selectUser(self, idusuario):
-  banco = Banco()
-  try:
- 
-      c = banco.conexao.cursor()
- 
-      c.execute("select * from usuarios where idusuario = " + idusuario + "  ")
- 
-      for linha in c:
-          self.idusuario = linha[0]
-          self.nome = linha[1]
-          self.telefone = linha[2]
-          self.email = linha[3]
-          self.usuario = linha[4]
-          self.senha = linha[5]
- 
-      c.close()
- 
-      return "Busca feita com sucesso!"
-  except:
-      return "Ocorreu um erro na busca do usuario"
+            return "Busca feita com sucesso!"
+        except:
+            return "Ocorreu um erro na busca do usuario"
